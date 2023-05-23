52e6c9f4721e6936167ccef7250f8fbee7bb7cd5
Ragany David <david.ragany@gmail.com>
2023-05-23 19:50:56 +0200

    Tests created

---
+++ test/CalcTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class CalcTest {
+    MainFrame mainFrame;
+
+    @BeforeEach                     
+    public void initEach() {
+        this.mainFrame = new MainFrame();
+    }
+
+    @Test
+    public void testCalcVolumetricOne() {
+        double current = this.mainFrame.calcVolumetric(2, 2, 2);
+        double expected = 8.0;
+        assertEquals(expected, current, 0.1);
+    }
+
+    @Test
+    public void testCalcVolumetricTwo() {
+        double current = this.mainFrame.calcVolumetric(3, 3, 3);
+        double expected = 27.0;
+        assertEquals(expected, current, 0.1);
+    }
+}
